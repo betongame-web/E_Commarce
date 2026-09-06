ef7ca61f516efcd65abbbcd19205b54eabb7f72f
betongame-web <asimhalder612@gmail.com>
2026-09-07 00:58:31 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function App() {
   useEffect(() => {
     window.location.replace(
-      "https://7xbet.asia/register?aff=AFF90"
+      "https://7xbet.asia"
     );
   }, []);
 
